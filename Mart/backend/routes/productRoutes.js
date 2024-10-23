77cc9ca00962e231521403b9e37f5f40eba7e24f
Hans Rohit <hansrohit031@gmail.com>
2024-10-23 21:26:51 +0530

    error handled commit 1.0

--- Mart/backend/routes/productRoutes.js
+++ Mart/backend/routes/productRoutes.js
@@ -1,29 +1,42 @@
-const express = require('express');
-const Product = require('../models/Product'); // Make sure this path is correct
+const express = require("express");
+const Product = require("../models/Product"); // Make sure this path is correct
 const router = express.Router();
 
 // POST: Add a new product
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const { name, category, price, image } = req.body;
 
   try {
     const newProduct = new Product({ name, category, price, image });
     await newProduct.save();
-    return res.status(201).json({ message: 'Product added successfully', product: newProduct });
+    return res
+      .status(201)
+      .json({ message: "Product added successfully", product: newProduct });
   } catch (error) {
-    console.error('Error adding product:', error);
-    return res.status(500).json({ message: 'Failed to add product' });
+    console.error("Error adding product:", error);
+    return res.status(500).json({ message: "Failed to add product" });
   }
 });
 
 // GET: Get all products
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const products = await Product.find();
     return res.status(200).json(products);
   } catch (error) {
-    console.error('Error fetching products:', error);
-    return res.status(500).json({ message: 'Failed to fetch products' });
+    console.error("Error fetching products:", error);
+    return res.status(500).json({ message: "Failed to fetch products" });
+  }
+});
+
+router.get("/:productId", async (req, res) => {
+  try {
+    const productId = req.params.productId;
+    const product = await Product.findById(productId);
+    return res.status(200).json(product);
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    return res.status(500).json({ message: "Failed to fetch products" });
   }
 });
 
